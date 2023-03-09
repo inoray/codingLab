1bb04344131b590da0f5ffc2b9e876da16f18d1b
PC-SHKIM\sungh <sunghunk@gmail.com>
2023-03-09 23:34:34 +0900

    [flutter/chatgpt] UI 구현, 사이드바, 메인 화면

--- flutter/chat_GPT_App/lib/const/color.dart
+++ flutter/chat_GPT_App/lib/const/color.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 final Color MAIN_BACKGROUND_COLOR = Color(0xFF343541);
 final Color SIDEBAR_BACKGROUND_COLOR = Color(0xFF202123);
 final Color SIDEBAR_COLOR = Color(0xFFf1f1f1);
+final Color SIDEBAR_LINE = Color(0xFF556556);
+final Color SIDEBAR_TEXT_COLOR = Colors.white;
--- flutter/chat_GPT_App/lib/main.dart
+++ flutter/chat_GPT_App/lib/main.dart
@@ -52,48 +52,93 @@ class _HomePageState extends State<HomePage> {
         children: [
           SideBar(),
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    Session sess = Session();
-                    final resp = await sess
-                        .post("https://api.openai.com/v1/chat/completions");
-                    print(resp);
-
-                    setState(() {
-                      this.txt = resp['choices'][0]['message']['content'];
-                    });
-                  },
-                  child: Text("chatgpt"),
-                ),
-                Row(
+                ListView(
+                  padding: EdgeInsets.zero,
                   children: [
-                    Expanded(
-                      // child: Markdown(
-                      //   data: this.txt,
-                      // ),
-                      child: DefaultTextStyle(
-                        style: const TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 16),
-                        child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            repeatForever: false,
-                            displayFullTextOnTap: true,
-                            totalRepeatCount: 1,
-                            animatedTexts: [
-                              TyperAnimatedText(
-                                this.txt.trim(),
-                              ),
-                            ]),
-                      ),
+                    ChatBox(
+                      content: "안녕하세요.",
+                      isChatGpt: false,
+                    ),
+                    ChatBox(
+                      content: "반갑습니다.\n어디세요?",
+                      isChatGpt: true,
                     ),
                   ],
                 ),
-                // Text(this.txt),
+                // Column(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     // ElevatedButton(
+                //     //   onPressed: () async {
+                //     //   },
+                //     //   child: Text("chatgpt"),
+                //     // ),
+                //     Row(
+                //       children: [
+                //         Expanded(
+                //           // child: Markdown(
+                //           //   data: this.txt,
+                //           // ),
+                //           child: DefaultTextStyle(
+                //             style: const TextStyle(
+                //               color: Colors.blue,
+                //               fontWeight: FontWeight.w700,
+                //               fontSize: 16,
+                //             ),
+                //             child: AnimatedTextKit(
+                //               isRepeatingAnimation: false,
+                //               repeatForever: false,
+                //               displayFullTextOnTap: true,
+                //               totalRepeatCount: 1,
+                //               animatedTexts: [
+                //                 TyperAnimatedText(
+                //                   this.txt.trim(),
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //       ],
+                //     ),
+                //     // Text(this.txt),
+                //   ],
+                // ),
+                Positioned.fill(
+                  bottom: 20,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          // labelText: 'Input',
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          suffixIcon: Container(
+                            child: IconButton(
+                              alignment: Alignment.centerRight,
+                              icon: Icon(Icons.send_outlined),
+                              onPressed: () async {
+                                Session sess = Session();
+                                final resp = await sess.post(
+                                    "https://api.openai.com/v1/chat/completions");
+                                print(resp);
+
+                                setState(() {
+                                  this.txt =
+                                      resp['choices'][0]['message']['content'];
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -106,8 +151,7 @@ class _HomePageState extends State<HomePage> {
 class Session {
   Map<String, String> headers = {
     'Content-Type': 'application/json',
-    'Authorization':
-        'Bearer $openaiKey',
+    'Authorization': 'Bearer $openaiKey',
   };
   Map<String, dynamic> body = {
     'model': 'gpt-3.5-turbo',
@@ -150,50 +194,156 @@ class SideBar extends StatelessWidget {
     return Drawer(
       width: 260,
       backgroundColor: SIDEBAR_BACKGROUND_COLOR,
-      child:
-      ListTileTheme(
-        contentPadding: const EdgeInsets.all(5),
-        iconColor: Colors.red,
-        textColor: Colors.black54,
-        tileColor: Colors.white,
-        style: ListTileStyle.list,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListTileTheme(
+              contentPadding: const EdgeInsets.only(left: 10.0),
+              iconColor: SIDEBAR_TEXT_COLOR,
+              textColor: SIDEBAR_TEXT_COLOR,
+              tileColor: SIDEBAR_BACKGROUND_COLOR,
+              // selectedColor: Colors.white,
+              // selectedTileColor: Colors.white,
+              style: ListTileStyle.list,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+                side: BorderSide(color: SIDEBAR_LINE),
+              ),
+              horizontalTitleGap: 5.0,
+              // minVerticalPadding: 6.0,
+              minLeadingWidth: 30.0,
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  SideBarCard(),
+                  SideBarCard(),
+                  SideBarCard(),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            child: Divider(
+              thickness: 1,
+              color: SIDEBAR_LINE,
+            ),
+          ),
+          SideBarButton(
+            icon: Icons.wb_sunny_outlined,
+            label: "Light mode",
+          ),
+          SideBarButton(
+            icon: Icons.person_outline,
+            label: "My account",
+          ),
+          SideBarButton(
+            icon: Icons.open_in_new_outlined,
+            label: "Updates & FAQ",
+          ),
+          SideBarButton(
+            icon: Icons.logout_outlined,
+            label: "Log out",
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-        ),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // TODO: Handle item 1 press
-              },
-            ),
-            Card(
-              margin: const EdgeInsets.all(10),
-              color: SIDEBAR_BACKGROUND_COLOR,
-              child: ListTile(
-                title: Text(
-                  "New chat",
-                  style: TextStyle(
-                    color: SIDEBAR_COLOR,
-                  ),
-                ),
-                onTap: () {
-                  // TODO: Handle item 2 press
-                },
-              ),
-            ),
-          ],
+class SideBarCard extends StatelessWidget {
+  const SideBarCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
+      color: SIDEBAR_BACKGROUND_COLOR,
+      child: SizedBox(
+        height: 50,
+        child: ListTile(
+          // visualDensity: VisualDensity(vertical: -4),
+          hoverColor: Color(0xFF2b2c2f),
+
+          leading: Icon(
+            Icons.add,
+            size: 20,
+          ),
+          title: Text(
+            "New chat",
+            style: TextStyle(fontSize: 14),
+            // textAlign: TextAlign.center,
+          ),
+          onTap: () {
+            // TODO: Handle item 2 press
+          },
         ),
       ),
+    );
+  }
+}
+
+class SideBarButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  const SideBarButton({
+    Key? key,
+    required this.label,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 8.0,
+        right: 8.0,
+        bottom: 4.0,
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        height: 50.0,
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Icon(icon),
+          label: Text(label),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: SIDEBAR_BACKGROUND_COLOR,
+            elevation: 0.0,
+            alignment: Alignment.centerLeft,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChatBox extends StatelessWidget {
+  final String content;
+  final bool isChatGpt;
+
+  const ChatBox({
+    Key? key,
+    required this.content,
+    required this.isChatGpt,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 50,
+        right: 50,
+        top: 30,
+        bottom: 30,
+      ),
+      child: Text(
+        content,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      color: isChatGpt ? Color(0xFF444654) : Colors.transparent,
     );
   }
 }
