a86e81518aee8b94f82ec868cac39aef20699f54
PC-SHKIM\sungh <sunghunk@gmail.com>
2023-04-07 01:08:22 +0900

    [flutter/chatgpt] ui 개선

--- flutter/chat_GPT_App/lib/const/color.dart
+++ flutter/chat_GPT_App/lib/const/color.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 final Color MAIN_BACKGROUND_COLOR = Color(0xFF343541);
+final Color MAIN_TEXT_COLOR = Color(0xffd1d5db);
 final Color SIDEBAR_BACKGROUND_COLOR = Color(0xFF202123);
 final Color SIDEBAR_COLOR = Color(0xFFf1f1f1);
-final Color SIDEBAR_LINE = Color(0xFF556556);
-final Color SIDEBAR_TEXT_COLOR = Colors.white;
+final Color SIDEBAR_LINE = Color(0xFF4C4C4F);
+final Color TEXTFILED_BACKGROUND_COLOR = Color(0xff40414f);
--- flutter/chat_GPT_App/lib/main.dart
+++ flutter/chat_GPT_App/lib/main.dart
@@ -5,8 +5,25 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chat_gpt_app/const/color.dart';
 import 'package:chat_gpt_app/const/key.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = const WindowOptions(
+    // size: Size(800, 600),
+    center: true,
+    // backgroundColor: Colors.transparent,
+    // skipTaskbar: false,
+    // titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+  windowManager.setTitle('ChatGPT');
   runApp(const MyApp());
 }
 
@@ -15,7 +32,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
@@ -30,12 +47,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String txt = "";
   late TextEditingController _controller;
 
   late List<dynamic> _messages;
   late FocusNode myFocusNode;
   late ScrollController _scrollController;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -48,19 +65,32 @@ class _HomePageState extends State<HomePage> {
     ];
     myFocusNode = FocusNode();
     _scrollController = ScrollController();
-    _scrollController.addListener(_scrollListener);
+    // _scrollController.addListener(_scrollListener);
   }
 
   _scrollListener() async {
-    if (_scrollController.offset == _scrollController.position.maxScrollExtent && !_scrollController.position.outOfRange){
+    if (_scrollController.offset ==
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
       //top
-    } else if (_scrollController.offset <= _scrollController.position.maxScrollExtent && !_scrollController.position.outOfRange){
+    } else if (_scrollController.offset <=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
       //bottom
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    // ListView 빌드가 완료된 후 자동으로 스크롤을 제일 아래로 이동 시킨다.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.fastOutSlowIn,
+      );
+    });
+
     return Scaffold(
       backgroundColor: MAIN_BACKGROUND_COLOR,
       // appBar: AppBar(
@@ -74,36 +104,43 @@ class _HomePageState extends State<HomePage> {
       //     SidebarXItem(icon: Icons.search, label: 'Search'),
       //   ],
       // ),
-      drawer: SideBar(),
+      // drawer: SideBar(),
 
       body: Row(
         children: [
-          SideBar(),
+          SideBar(
+            onPressedNewChat: () {
+              setState(() {
+                _messages = [
+                  {"role": "system", "content": "You are a helpful assistant."},
+                ];
+              });
+            },
+          ),
           Expanded(
             child: Stack(
               alignment: Alignment.center,
               children: [
-                Column(
-                  children: [
-                    ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      controller: _scrollController,
-                      padding: EdgeInsets.zero,
-                      itemCount: _messages.length,
-                      itemBuilder: (
-                        BuildContext context,
-                        int index,
-                      ) {
-                        if (index == 0) return SizedBox();
-                        return ChatBox(
-                          content: _messages[index]['content'],
-                          isChatGpt:
-                              _messages[index]['role'] == 'user' ? false : true,
-                        );
-                      },
-                    ),
-                  ],
+                Positioned.fill(
+                  bottom: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    controller: _scrollController,
+                    padding: EdgeInsets.zero,
+                    itemCount: _messages.length,
+                    itemBuilder: (
+                      BuildContext context,
+                      int index,
+                    ) {
+                      if (index == 0) return SizedBox();
+                      return ChatBox(
+                        content: _messages[index]['content'],
+                        isChatGpt:
+                            _messages[index]['role'] == 'user' ? false : true,
+                      );
+                    },
+                  ),
                 ),
                 // Column(
                 //   mainAxisAlignment: MainAxisAlignment.center,
@@ -148,27 +185,52 @@ class _HomePageState extends State<HomePage> {
                   child: Align(
                     alignment: Alignment.bottomCenter,
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(10.0),
                       child: TextField(
+                        // enabled: _isLoading ? false : true,
+                        readOnly: _isLoading ? true : false,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
                         autofocus: true,
                         focusNode: myFocusNode,
                         textInputAction: TextInputAction.done,
                         onSubmitted: (value) async {
                           request();
                         },
+                        style: TextStyle(color: MAIN_TEXT_COLOR),
+                        cursorColor: Colors.white,
                         controller: _controller,
                         decoration: InputDecoration(
                           // labelText: 'Input',
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(),
-                          suffixIcon: Container(
-                            child: IconButton(
-                              alignment: Alignment.centerRight,
-                              icon: Icon(Icons.send_outlined),
-                              onPressed: () async {
-                                request();
-                              },
-                            ),
+                          filled: true,
+                          fillColor: TEXTFILED_BACKGROUND_COLOR,
+                          // border: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 1, color: Color(0xff40414f)),
+                          // ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 0, color: TEXTFILED_BACKGROUND_COLOR),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 0, color: TEXTFILED_BACKGROUND_COLOR),
+                          ),
+                          suffixIcon: SizedBox(
+                            width: 10,
+                            height: 10,
+                            child: _isLoading
+                                ? SpinKitThreeBounce(
+                                    color: MAIN_TEXT_COLOR,
+                                    size: 15.0,
+                                  )
+                                : IconButton(
+                                    alignment: Alignment.centerRight,
+                                    icon: const Icon(Icons.send_outlined),
+                                    color: MAIN_TEXT_COLOR,
+                                    onPressed: () async {
+                                      request();
+                                    },
+                                  ),
                           ),
                         ),
                       ),
@@ -184,33 +246,32 @@ class _HomePageState extends State<HomePage> {
   }
 
   void request() async {
-    this.txt = _controller.text;
-    // print(this._messages);
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _isLoading = true;
+        _messages.add(
+          {'role': "user", 'content': _controller.text},
+        );
+      });
 
-    this._messages.add(
-      {'role': "user", 'content': _controller.text},
-    );
-    // print(this._messages);
+      _controller.clear();
+      Session sess = Session();
+      final resp = await sess.post(
+          "https://api.openai.com/v1/chat/completions", _messages);
 
-    Session sess = Session();
-    final resp = await sess.post(
-        "https://api.openai.com/v1/chat/completions", this._messages);
-    // print(resp);
+      print(resp);
 
-    this._messages.add(
-      {
-        'role': "assistant",
-        'content': resp['choices'][0]['message']['content']
-      },
-    );
-
-    // print(this._messages);
-
-    setState(() {
-      // this.txt =
-      //     resp['choices'][0]['message']['content'];
-    });
-    _controller.clear();
+      setState(() {
+        _isLoading = false;
+        _messages.add(
+          {
+            'role': "assistant",
+            'content': resp['choices'][0]['message']['content']
+          },
+        );
+      });
+    }
+    // ignore: use_build_context_synchronously
     FocusScope.of(context).requestFocus(myFocusNode);
   }
 }
@@ -222,17 +283,11 @@ class Session {
       'Authorization': 'Bearer $openaiKey',
     };
 
-    // List<dynamic> messages = [
-    //   {"role": "system", "content": "You are a helpful assistant."},
-    //   {"role": "user", "content": "안녕"},
-    // ];
-
     Map<String, dynamic> body = {
       'model': 'gpt-3.5-turbo',
+      // 'model': 'gpt-4-32k-0314',
       'messages': messages,
     };
-
-    print('get() url: $strUrl');
 
     var url = Uri.https(
       "api.openai.com",
@@ -244,6 +299,21 @@ class Session {
       headers: headers,
       body: jsonEncode(body),
     );
+
+    // Map<String, String> headers2 = {
+    //   'Authorization': 'Bearer $openaiKey',
+    // };
+    //
+    // var url2 = Uri.https(
+    //   "api.openai.com",
+    //   "/v1/models",
+    // );
+    //
+    // final resp = await http.get(
+    //   url2,
+    //   headers: headers2,
+    // );
+    // print(resp);
 
     final int statusCode = resp.statusCode;
     if (statusCode < 200 || statusCode > 400) {
@@ -258,7 +328,12 @@ class Session {
 }
 
 class SideBar extends StatelessWidget {
-  const SideBar({Key? key}) : super(key: key);
+  final VoidCallback onPressedNewChat;
+
+  const SideBar({
+    Key? key,
+    required this.onPressedNewChat,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -267,11 +342,18 @@ class SideBar extends StatelessWidget {
       backgroundColor: SIDEBAR_BACKGROUND_COLOR,
       child: Column(
         children: [
+          const SizedBox(height: 10.0),
+          SideBarButton(
+            icon: Icons.add,
+            label: "New chat",
+            isBorder: true,
+            onPressed: onPressedNewChat,
+          ),
           Expanded(
             child: ListTileTheme(
               contentPadding: const EdgeInsets.only(left: 10.0),
-              iconColor: SIDEBAR_TEXT_COLOR,
-              textColor: SIDEBAR_TEXT_COLOR,
+              iconColor: MAIN_TEXT_COLOR,
+              textColor: MAIN_TEXT_COLOR,
               tileColor: SIDEBAR_BACKGROUND_COLOR,
               // selectedColor: Colors.white,
               // selectedTileColor: Colors.white,
@@ -286,7 +368,7 @@ class SideBar extends StatelessWidget {
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
-                  SideBarAddButton(),
+                  //SideBarAddButton(),
                 ],
               ),
             ),
@@ -301,52 +383,28 @@ class SideBar extends StatelessWidget {
           SideBarButton(
             icon: Icons.wb_sunny_outlined,
             label: "Light mode",
+            isBorder: false,
+            onPressed: () {},
           ),
           SideBarButton(
             icon: Icons.person_outline,
             label: "My account",
+            isBorder: false,
+            onPressed: () {},
           ),
           SideBarButton(
             icon: Icons.open_in_new_outlined,
             label: "Updates & FAQ",
+            isBorder: false,
+            onPressed: () {},
           ),
           SideBarButton(
             icon: Icons.logout_outlined,
             label: "Log out",
+            isBorder: false,
+            onPressed: () {},
           ),
         ],
-      ),
-    );
-  }
-}
-
-class SideBarAddButton extends StatelessWidget {
-  const SideBarAddButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
-      color: SIDEBAR_BACKGROUND_COLOR,
-      child: SizedBox(
-        height: 50,
-        child: ListTile(
-          // visualDensity: VisualDensity(vertical: -4),
-          hoverColor: Color(0xFF2b2c2f),
-
-          leading: Icon(
-            Icons.add,
-            size: 20,
-          ),
-          title: Text(
-            "New chat",
-            style: TextStyle(fontSize: 14),
-            // textAlign: TextAlign.center,
-          ),
-          onTap: () {
-            // TODO: Handle item 2 press
-          },
-        ),
       ),
     );
   }
@@ -366,14 +424,13 @@ class SideBarCard extends StatelessWidget {
           // visualDensity: VisualDensity(vertical: -4),
           hoverColor: Color(0xFF2b2c2f),
 
-          leading: Icon(
+          leading: const Icon(
             Icons.add,
             size: 20,
           ),
-          title: Text(
+          title: const Text(
             "New chat",
             style: TextStyle(fontSize: 14),
-            // textAlign: TextAlign.center,
           ),
           onTap: () {
             // TODO: Handle item 2 press
@@ -387,10 +444,16 @@ class SideBarCard extends StatelessWidget {
 class SideBarButton extends StatelessWidget {
   final String label;
   final IconData icon;
+  final bool isBorder;
+  final VoidCallback onPressed;
+  //typedef VoidCallback = void Function()
+
   const SideBarButton({
     Key? key,
     required this.label,
     required this.icon,
+    required this.isBorder,
+    required this.onPressed,
   }) : super(key: key);
 
   @override
@@ -405,13 +468,23 @@ class SideBarButton extends StatelessWidget {
         width: double.infinity,
         height: 50.0,
         child: ElevatedButton.icon(
-          onPressed: () {},
-          icon: Icon(icon),
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: 20,
+          ),
           label: Text(label),
           style: ElevatedButton.styleFrom(
             backgroundColor: SIDEBAR_BACKGROUND_COLOR,
             elevation: 0.0,
             alignment: Alignment.centerLeft,
+            foregroundColor: MAIN_TEXT_COLOR,
+            side: isBorder
+                ? BorderSide(
+                    width: 1.0,
+                    color: SIDEBAR_LINE,
+                  )
+                : null,
           ),
         ),
       ),
@@ -432,19 +505,32 @@ class ChatBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(
+      padding: const EdgeInsets.only(
         left: 50,
         right: 50,
         top: 30,
         bottom: 30,
       ),
-      child: SelectableText(
-        content,
-        style: TextStyle(
-          color: Colors.white,
-        ),
-      ),
       color: isChatGpt ? Color(0xFF444654) : Colors.transparent,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            isChatGpt ? "asset/chatgpt-icon.png" : 'asset/user.png',
+            width: 40.0,
+          ),
+          const SizedBox(width: 20.0),
+          Expanded(
+            child: SelectableText(
+              content,
+              style: TextStyle(
+                color: MAIN_TEXT_COLOR,
+                fontSize: 17.0,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
