a047af8afdc950ca0d187dad7047dc43fc9b497d
shkim <shkim@inzisoft.com>
2024-08-14 16:27:01 +0900

    [20240730 wpf] mvvm 실습
    - textBox, button

---
+++ 20240730_wpf/WpfApp3/Models/MainModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Models
+{
+    public class MainModel : ObservableObject
+    {
+        private int inputDigit;
+        public int InputDigit
+        {
+            get => inputDigit;
+            set
+            {
+                //OutputDigit = value * 2;
+                SetProperty(ref inputDigit, value);
+            }
+        }
+
+        private int outputDigit;
+        public int OutputDigit
+        {
+            get => outputDigit;
+            set => SetProperty(ref outputDigit, value);
+        }
+
+    }
+}
---
+++ 20240730_wpf/WpfApp3/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp3.Models;
+
+namespace WpfApp3.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private MainModel mainModel = new MainModel();
+        public MainModel MainModel
+        {
+            get => mainModel;
+            set => SetProperty(ref mainModel, value);
+        }
+
+        public RelayCommand ButtonClick { get; }
+
+        public MainViewModel()
+        {
+            ButtonClick = new RelayCommand(ChangeDigit);
+        }
+
+        // 명령 실행 메서드
+        private void ChangeDigit()
+        {
+            MainModel.OutputDigit = MainModel.InputDigit * 2;
+        }
+    }
+}
